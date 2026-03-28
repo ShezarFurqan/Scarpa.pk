4e4535e416175df01ae0a9b0675264e579819181
Shezar Furqan <webweavers883@gmail.com>
2026-03-28 20:02:53 +0500

    sitemap fix

--- src/app/sitemap.xml.js
+++ src/app/sitemap.xml.js
@@ -1,41 +1,34 @@
+Your Sitemap does not contain any URLs. Please validate and resubmit your Sitemap// app/sitemap.xml.js
 import { db } from "@/app/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
-export const runtime = "edge"; // Fastest response
-
 export async function GET() {
-  const productsCol = collection(db, "products");
-  const productsSnap = await getDocs(productsCol);
+  const productsSnap = await getDocs(collection(db, "products"));
+  const products = productsSnap.docs.map(doc => doc.data());
 
-  const baseUrl = "https://scarpa.pk";
-  const today = new Date().toISOString();
+  const urls = [
+    { loc: "https://scarpa.pk/", priority: 1.0 },
+    ...products.map(p => ({
+      loc: `https://scarpa.pk/product/${p.slug}-${p.id}`,
+      priority: 0.8
+    }))
+  ];
 
-  let urls = `
-    <url>
-      <loc>${baseUrl}</loc>
-      <lastmod>${today}</lastmod>
-    </url>
-  `;
-
-  productsSnap.forEach((doc) => {
-    const product = doc.data();
-    const slug = `${product.title.toLowerCase().replace(/\s+/g, "-")}-${doc.id}`;
-    urls += `
-      <url>
-        <loc>${baseUrl}/product/${slug}</loc>
-        <lastmod>${today}</lastmod>
-      </url>
-    `;
-  });
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls}
+    ${urls
+      .map(
+        url => `
+      <url>
+        <loc>${url.loc}</loc>
+        <priority>${url.priority}</priority>
+      </url>
+    `
+      )
+      .join("")}
   </urlset>`;
 
-  return new Response(sitemap, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
+  return new Response(xml, {
+    headers: { "Content-Type": "application/xml" },
   });
 }
