3e928ba4cd5af469c4ced7024ae1c4b898f861f2
Shezar Furqan <webweavers883@gmail.com>
2026-03-28 20:10:37 +0500

    sitemap fixed

--- src/app/sitemap.xml.js
+++ src/app/sitemap.xml.js
@@ -24,6 +24,8 @@ export async function GET() {
     }))
   ];
 
+  console.log(urls)
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${urls
